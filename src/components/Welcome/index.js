08654c373ef4a96977a2d8b3242ac80d7da87b3c
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-06-20 19:54:00 -0300

    fix bug componente welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Style from "./styles/style.css";
 import Psi from "./images/psiwelcome.png"
 import Clinico from "./images/saudewelcome.png"
 import Medico from "./images/medicowelcome.png"
@@ -27,34 +28,34 @@ const Welcome = () => {
 
 
       <div className='contents'>
-      <div className='contentBroken'>
-        <img src={Clinico} className='img' />
-        <h1>Clínico Geral</h1>
+        <div className='contentBroken'>
+          <img src={Clinico} className='img' />
+          <h1>Clínico Geral</h1>
 
-        <p>Faça sua consulta a qualquer momento, 24 horas por dia. Sem limites e sem carência.</p>
-      </div>
-      </div>
-
-      <div className='contents'>
-      <div className='contentBroken'>
-        <img src={Medico} className='img' />
-
-        <h1>Médico especialista</h1>
-
-        <p>Consultas agendadas, ilimitadas, sem custos extras e sem carência.</p>
+          <p>Faça sua consulta a qualquer momento, 24 horas por dia. Sem limites e sem carência.</p>
         </div>
       </div>
 
       <div className='contents'>
-      <div className='contentBroken'>
-        <img src={Nutri} className='img' />
+        <div className='contentBroken'>
+          <img src={Medico} className='img' />
 
-        <h1>Nutricionista</h1>
+          <h1>Médico especialista</h1>
 
-        <p>Dieta personalizada para atender suas necessidades em qualquer fase da vida, sem custos extras e sem carência.</p>
+          <p>Consultas agendadas, ilimitadas, sem custos extras e sem carência.</p>
+        </div>
       </div>
+
+      <div className='contents'>
+        <div className='contentBroken'>
+          <img src={Nutri} className='img' />
+
+          <h1>Nutricionista</h1>
+
+          <p>Dieta personalizada para atender suas necessidades em qualquer fase da vida, sem custos extras e sem carência.</p>
+        </div>
       </div>
-      </div>
+    </div>
 
   )
 }
